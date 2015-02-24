568289e2f35de17a86423a92c0cd23a88710e699
Rev Dave Wakely <dave@gurucomputers.co.uk>
2015-02-24 23:51:41 +0000

    Add Web Dev Content

--- webdev.php
+++ webdev.php
@@ -26,16 +26,46 @@
             <div class="col-md-20">
                 <div class="panel panel-info">
                     <div class="panel-heading"><h1>One Website for All Devices</h1></div>
-                    <div class="panel-body">
+                    <div class="panel-body text-center">
                         <h2>Responsive Website Design</h2>
                         <p>In Today's Technology Advanced World, people are more and more likely to access your website on Phones, Tablets and other Portable Devices.</p>
                         <p>Having a website that dynamically adjusts the user's experience to match their device, your showing your Brand's Attention to Detail.</p>
+                        <p>Here at Guru Computers Ltd, we operate on a Mobile-First Approach. This means your website is fully optimized for Mobile Phones, and will look at home on all devices!</p>
                     </div>
                 </div>
             </div>
-            <div class="col-md-6"></div>
-            <div class="col-md-6"></div>
-            <div class="col-md-6"></div>
+        </div>
+        <div class="row">
+            <div class="col-md-6 col-md-offset-1">
+                <div class="panel panel-info">
+                    <div class="panel-heading">Design</div>
+                    <div class="panel-body text-center">
+                        <p>Our design mockups and wireframes are presented in Mobile, Tablet & Desktop Formats.</p>
+                        <p>These mockups allow us to work with you to make sure your visitors don't miss any important information when they use a smaller device.</p>
+                        <p>We then adapt the website between these device designs to make sure a 7" tablet shows the same as a 10.1" so your website is truely fluid and responsive.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="panel panel-info">
+                    <div class="panel-heading">Build</div>
+                    <div class="panel-body text-center">
+                        <p>Before we get stuck into coding your website, we ensure we have developed a secure, high performance platform for your website to run on.</p>
+                        <p>We utilize the latest CSS3, SASS, LESS, HTML5 and jQuery Techniques to ensure your website works well on any device.</p>
+                        <p>We also make sure that all Fonts, Images and other User Interface Elements are responsive to the best standard current web technologies allow for.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="panel panel-info">
+                    <div class="panel-heading">Test</div>
+                    <div class="panel-body text-center">
+                        <p>Your website is only a success when it can be navigated with ease, so we perform rigourous testing throughout the development process.</p>
+                        <p>We test your website thoughout the build on several different browsers and devices to ensure maxiumum compatibility. Then we check to make sure there are no vulnerabilities!</p>
+                        <p>During this testing phase, we may invite you to test the website yourselves for ease-of-use.</p>
+                    </div>
+                </div>
+            </div>
         </div>
   	</div>
 
