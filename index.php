7bea183861c1cb5022faf57760d1498ccbc57f47
Rev Neil Wakely <neil@gurucomputers.co.uk>
2015-02-24 20:13:50 +0000

    cd.php added to index.php

--- index.php
+++ index.php
@@ -68,7 +68,7 @@
         			<li class="dropdown">
         				<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Printing <span class="caret"></span></a>
         				<ul class="dropdown-menu" role="menu">
-        					<li><a href="#">CD Printing & Duplication</a></li>
+        					<li><a href="cd.php">CD Printing & Duplication</a></li>
         					<li class="divider"></li>
         					<li><a href="#">Flyers</a></li>
         					<li><a href="#">Business Cards</a></li>
