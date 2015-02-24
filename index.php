80678d63573e9cb8d7607b0ca86c5d165fa13099
Rev Neil Wakely <neil@gurucomputers.co.uk>
2015-02-24 21:17:37 +0000

    changed footer and nav in index.php

--- index.php
+++ index.php
@@ -17,80 +17,7 @@
     <![endif]-->
   </head>
   <body>
-  	<nav class="navbar navbar-default">
-  		<div class="container-fluid">
-    		<div class="navbar-header">
-    			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-    				<span class="sr-only">Toggle navigation</span>
-    				<span class="icon-bar"></span>
-    				<span class="icon-bar"></span>
-    				<span class="icon-bar"></span>
-    			</button>
-      			<a class="navbar-brand" href="#">
-        			<img alt="Guru Computers Ltd" src="img/logo.png" class="navbar-logo">
-        		</a>
-        	</div>
-        	<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-        		<ul class="nav navbar-nav nav-pills">
-        			<li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
-        			<li><a href="#">About</a></li>
-        			<li class="dropdown">
-        				<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Contact Us <span class="caret"></span></a>
-        				<ul class="dropdown-menu" role="menu">
-        					<li><a href="tel:07942759689"><span class="glyphicon glyphicon-earphone" aria-hidden="true"></span> Dave (07942 759689)</a></li>
-        					<li><a href="tel:07942759739"><span class="glyphicon glyphicon-earphone" aria-hidden="true"></span> Neil (07942 759739)</a></li>
-        					<li class="divider"></li>
-        					<li><a href="mailto:sales@gurucomputers.co.uk"><img src="./img/email.jpg" class="menu-icons">Email Us</a></li>
-        					<li class="divider"></li>
-        					<li><a href="http:///www.twitter.com/gurucomputersuk" target="_blank"><img src="./img/twitter.png" class="menu-icons">Follow us on Twitter</a></li>
-        					<li class="divider"></li>
-        					<li><a href="http:///www.facebook.com/gurucomputersuk" target="_blank"><img src="./img/facebook.png" class="menu-icons">Add us on Facebook</a></li>
-        				</ul>
-        			</li>
-        			<li class="dropdown">
-        				<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Repairs <span class="caret"></span></a>
-        				<ul class="dropdown-menu" role="menu">
-        					<li><a href="#">Computer Repairs</a></li>
-        					<li><a href="#">Mobile Repairs</a></li>
-        					<li class="divider"></li>
-        					<li><a href="#">Book Now</a></li>
-        				</ul>
-        			</li>
-        			<li class="dropdown">
-        				<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Websites <span class="caret"></span></a>
-        				<ul class="dropdown-menu" role="menu">
-        					<li><a href="#">Website Development</a></li>
-        					<li><a href="#">Website Hosting</a></li>
-        					<li class="divider"></li>
-        					<li><a href="#">Our Portfolio</a></li>
-        				</ul>
-        			</li>
-        			<li class="dropdown">
-        				<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Printing <span class="caret"></span></a>
-        				<ul class="dropdown-menu" role="menu">
-        					<li><a href="cd.php">CD Printing & Duplication</a></li>
-        					<li class="divider"></li>
-        					<li><a href="#">Flyers</a></li>
-        					<li><a href="#">Business Cards</a></li>
-        					<li><a href="#">Letterheads</a></li>
-        					<li class="divider"></li>
-        					<li><a href="#">General Printing</a></li>
-        					<li class="divider"></li>
-        					<li><a href="#">Our Portfolio</a></li>
-        				</ul>
-        			</li>
-					<li><a href="http://www.gurucomputers.co.uk/shop" target="_blank">Shop</a></li>
-					<li><a href="http://www.gurucomputers.co.uk/blog" target="_blank">Blog</a></li>
-					<li class="dropdown">
-        				<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Testimonials <span class="caret"></span></a>
-        				<ul class="dropdown-menu" role="menu">
-        					<li><a href="#">View Testimonials</a></li>
-        					<li><a href="#">Add a Testimonial</a></li>
-        				</ul>
-        			</li>
-        		</ul>
-        </div>
-	</nav>
+  	<?php include "nav.php"; ?>
     <div class="wrapper">
   		<div class="container">
   			<div class="row clearfix">
@@ -187,39 +114,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
-    <footer class="footer">
-        <center>
-          <ul>
-            <li>
-              <?php
-                $start=2014;
-                $current=date("Y");
-                $cy="";
-                if ($current==$start) {
-                  $cy=$start;
-                }
-                if ($current>$start) {
-                  $cy=$start."-".$current;
-                }
-                echo "&copy"." ".$cy." Guru Computers Ltd."."\n"; 
-              ?>
-            </li>
-            <li>
-             <a href="mailto:sales@gurucomputers.co.uk"><img src="./img/email.jpg" class="icons">Email Us</a>
-            </li>
-            <li>
-              <a href="https://www.youtube.com/user/gurucomputersuk" target="_blank"><img src="./img/youtube.png" class="icons">Guru Channel</a>
-            </li>
-            <li>
-              <a href="https://www.facebook.com/GuruComputersUK" target="_blank"><img src="./img/facebook.png" class="icons">Guru Facebook</a>
-            </li>
-            <li>
-              <a href="https://twitter.com/GuruComputersUK" target="_blank"><img src="./img/twitter.png" class="icons">Guru Twitter</a>
-            </li>
-          </ul>
-        </center>
-      </div>
-    </footer>
+    <?php include "footer.php"; ?>
 
   </body>
 </html>
