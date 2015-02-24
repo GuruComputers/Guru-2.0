9882b5805573170ae0ceb1eae67ebbfe7c701019
Rev Dave Wakely <dave@gurucomputers.co.uk>
2015-02-24 15:32:01 +0000

    Make Logo Responsive Please

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#"><img src="img/logo.png" class="nav-logo"></a>
+          <a class="navbar-brand" href="#"><img src="img/logo.png" class="img-responsive"></a>
         </div>
         <div id="navbar" class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
