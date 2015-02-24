05f4b776f2c95c01cf4b073d216f0635a3b97c1a
Rev Dave Wakely <dave@gurucomputers.co.uk>
2015-02-24 19:50:36 +0000

    Add Twitter Box

--- index.php
+++ index.php
@@ -176,25 +176,8 @@
 						</div>
 					</div>
 					<div class="col-md-4 column">
-						<div class="panel panel-info">
-							<div class="panel-heading">Whatever your printing needs, we've got you covered!</div>
-							<div class="panel-body">
-								<p>Since day zero, we have consistently developed our Printed Product range to provide the best choice, value, and technological opportunities for our customers. We have a wide and varied range of over 100 media types from CD and DVD to Flyers and Promotional Products.</p>
-								<p>We have direct relationships with some of the world’s leading manufacturers, and are UK distributors for a number of brands.</p>
-								<p>Our Printing Range includes:-</p>
-								<ul>
-									<li>CD / DVD On-Body Printing</li>
-									<li>Business Cards</li>
-									<li>Leaflets & Flyers</li>
-									<li>A5 Booklets</li>
-									<li>Litho Posters</li>
-									<li>Outdoor PVC Banners</li>
-									<li>Promoter Packs</li>
-									<li>Bar Menus</li>
-									<li>And Much Much More!</li>
-								</ul>
-							</div>
-						</div>
+						<a class="twitter-timeline"  href="https://twitter.com/GuruComputersUK" data-widget-id="570309326427148288">Tweets by @GuruComputersUK</a>
+						<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 					</div>
 				</div>
 			</div>
