f8e1aa1c989d0a9eac484d903357199a77010558
Rev Dave Wakely <dave@gurucomputers.co.uk>
2015-02-24 16:20:46 +0000

    Add Nav Pills

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
         		</a>
         	</div>
         	<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-        		<ul class="nav navbar-nav">
+        		<ul class="nav navbar-nav nav-pills">
         			<li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
         			<li><a href="#">Link</a></li>
         			<li class="dropdown">
