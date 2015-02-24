7e7409651d02857f063a41c15719f662ad0fb97c
Rev Dave Wakely <dave@gurucomputers.co.uk>
2015-02-24 19:18:34 +0000

    Add Computer Repairs Text

--- index.php
+++ index.php
@@ -103,7 +103,7 @@
 				<div class="col-md-20 column">
 					<div class="row clearfix">
 						<div class="col-md-5 column">
-							<div class="panel panel-default">
+							<div class="panel panel-info">
 								<div class="panel-heading">Need a New Website?</div>
 								<div class="panel-body">
 									<p>Our In-House Team handle all regular aspects of designing, hand-coding and hosting your website so you can relax knowing your Web Presence is in safe hands!</P>
@@ -119,6 +119,22 @@
 							</div>
 						</div>
 						<div class="col-md-5 column">
+							<div class="panel panel-info">
+								<div class="panel-heading">Computer Running Slow?</div>
+								<div class="panel-body">
+									<p>Here at Guru Computers Ltd we never put a healthy computer down!</p>
+									<p>We have been busting Computer Viruses as a team for the best part of 17 years, and we aren't going to stop now! With the use of Multi-layered Antivirus Technology, we can provide fast and effective protection against current and future threats.</p>
+									<p>The bottom line is we keep you safe and we keep it simple! Our range of services include:- </p>
+									<ul>
+										<li>Virus Removals</li>
+										<li>PC Upgrades</li>
+										<li>Reinstalls</li>
+										<li>Laptop Screen Replacements</li>
+										<li>Remote Support</li>
+										<li>Data Recovery</li>
+									</ul>
+								</div>
+							</div>
 						</div>
 						<div class="col-md-5 column">
 						</div>
