0e6c6446b1c0717f2e7310fa4abad900cf93f579
Rev Dave Wakely <dave@gurucomputers.co.uk>
2015-02-24 21:30:51 +0000

    PayPal Button Bug Fix

--- hosting.php
+++ hosting.php
@@ -111,28 +111,39 @@
                                 <li class="list-group-item"><i class="icon-ok text-danger"></i> Choice of Linux OS</li>
                             </ul>
                             <div class="panel-footer">
-                                <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                                <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" class="hosting">
                                     <input type="hidden" name="cmd" value="_s-xclick">
                                     <input type="hidden" name="hosted_button_id" value="J3H3SPKGH7BXL">
                                     <table>
                                         <tr>
-                                            <td>
+                                            <td style="color: black">
                                                 <input type="hidden" name="on0" value="Management Options">Management Options
                                             </td>
                                         </tr>
                                         <tr>
-                                            <td>
+                                            <td style="color: black">
                                                 <select name="os0">
                                                     <option value="Managed">Managed : £10.00 GBP - monthly</option>
                                                     <option value="Unmanaged">Unmanaged : £5.00 GBP - monthly</option>
                                                 </select> 
                                             </td>
                                         </tr>
+                                        <tr>
+                                            <td style="color: black">
+                                                <input type="hidden" name="on1" value="Domain Name">Domain Name
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td style="color: black">
+                                                <input type="text" name="os1" maxlength="200">
+                                            </td>
+                                        </tr>
                                     </table>
                                     <input type="hidden" name="currency_code" value="GBP">
-                                    <input type="image" src="https://www.paypalobjects.com/en_US/GB/i/btn/btn_subscribeCC_LG.gif" border="0" name="submit" alt="PayPal – The safer, easier way to pay online.">
+                                    <input type="image" src="https://www.paypalobjects.com/en_US/GB/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal – The safer, easier way to pay online.">
                                     <img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1">
                                 </form>
+
                             </div>
                         </div>
                         <!-- /PRICE ITEM -->
