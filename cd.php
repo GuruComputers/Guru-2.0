1962723c4ae3e2e3467f62b68a4d07c0fe07b40d
Rev Neil Wakely <neil@gurucomputers.co.uk>
2015-02-24 20:44:05 +0000

    created cd.css

--- cd.php
+++ cd.php
@@ -7,7 +7,7 @@
     <title>Guru Computers Ltd</title>
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/custom.css" rel="stylesheet">
+    <link href="css/cd.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -132,7 +132,7 @@
                             </div>
                         </div>
                     </div>
-                    
+
                 </div>
             </div>
         </div>
