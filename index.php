6db0e3a0478b4eeedecff504530c7a327e11b217
Rev Dave Wakely <dave@gurucomputers.co.uk>
2015-02-24 17:41:02 +0000

    Printing Menu Bugfix

--- index.php
+++ index.php
@@ -55,10 +55,12 @@
         				<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Printing <span class="caret"></span></a>
         				<ul class="dropdown-menu" role="menu">
         					<li><a href="#">CD Printing & Duplication</a></li>
-        					<li><a href="#">Flyer Printing</a></li>
-        					<li class="divider">Business card Printing</li>
-        					<li><a href="#">Letterhead Printing</a></li>
-        					<li class="divider">General Printing</li>
+        					<li class="divider"></li>
+        					<li><a href="#">Flyers</a></li>
+        					<li><a href="#"></a>Business Cards</a></li>
+        					<li><a href="#">Letterheads</a></li>
+        					<li class="divider"></li>
+        					<li><a href="#">General Printing</a></li>
         				</ul>
         			</li>
 					<li><a href="#">Shop</a></li>
