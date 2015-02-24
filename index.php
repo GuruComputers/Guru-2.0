d096e16668bee84adbcb84831e83ca642c2fa043
Rev Dave Wakely <dave@gurucomputers.co.uk>
2015-02-24 15:24:57 +0000

    Add Custom CSS

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#"><img src="img/logo.png"></a>
+          <a class="navbar-brand" href="#"><img src="img/logo.png" class="nav-logo"></a>
         </div>
         <div id="navbar" class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
@@ -39,6 +39,7 @@
     </nav>
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
     <link href="css/footer.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
