bdf34ef9d13241c9b3a0a582d1b4b6c7cb8b769a
Rev Neil Wakely <neil@gurucomputers.co.uk>
2015-02-24 21:15:33 +0000

    removed footer to separate file

--- cd.php
+++ cd.php
@@ -71,39 +71,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
-    <footer class="footer">
-        <center>
-          <ul>
-            <li>
-              <?php
-                $start=2014;
-                $current=date("Y");
-                $cy="";
-                if ($current==$start) {
-                  $cy=$start;
-                }
-                if ($current>$start) {
-                  $cy=$start."-".$current;
-                }
-                echo "&copy"." ".$cy." Guru Computers Ltd."."\n"; 
-              ?>
-            </li>
-            <li>
-             <a href="mailto:sales@gurucomputers.co.uk"><img src="./img/email.jpg" class="icons">Email Us</a>
-            </li>
-            <li>
-              <a href="https://www.youtube.com/user/gurucomputersuk" target="_blank"><img src="./img/youtube.png" class="icons">Guru Channel</a>
-            </li>
-            <li>
-              <a href="https://www.facebook.com/GuruComputersUK" target="_blank"><img src="./img/facebook.png" class="icons">Guru Facebook</a>
-            </li>
-            <li>
-              <a href="https://twitter.com/GuruComputersUK" target="_blank"><img src="./img/twitter.png" class="icons">Guru Twitter</a>
-            </li>
-          </ul>
-        </center>
-      </div>
-    </footer>
+    <?php include "footer.php"; ?>
 
   </body>
 </html>
