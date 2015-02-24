6564c1b010d7b652dc5b32c334e25f8e28b89387
Rev Dave Wakely <dave@gurucomputers.co.uk>
2015-02-24 23:18:02 +0000

    Add Web Dev Page

---
+++ webdev.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Guru Computers Ltd</title>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/hosting.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <?php 
+        $page = "websites";
+        include "nav.php"; 
+    ?>
+    <div class="wrapper">
+        <div class="row">
+            <div class="col-md-20">
+                <div class="panel panel-info">
+                    <div class="panel-heading"><h1>One Website for All Devices</h1></div>
+                    <div class="panel-body">
+                        <h2>Responsive Website Design</h2>
+                        <p>In Today's Technology Advanced World, people are more and more likely to access your website on Phones, Tablets and other Portable Devices.</p>
+                        <p>Having a website that dynamically adjusts the user's experience to match their device, your showing your Brand's Attention to Detail.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6"></div>
+            <div class="col-md-6"></div>
+            <div class="col-md-6"></div>
+        </div>
+  	</div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+    <?php include "footer.php"; ?>
+  </body>
+</html>
