c566c2e1fe40773ab729c3666cfde385070cce7c
Rev Dave Wakely <dave@gurucomputers.co.uk>
2015-02-24 16:34:27 +0000

    Add our Phone Numbers

--- index.php
+++ index.php
@@ -37,8 +37,8 @@
         			<li class="dropdown">
         				<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Contact Us <span class="caret"></span></a>
         				<ul class="dropdown-menu" role="menu">
-        					<li><a href="#">Phone Dave</a></li>
-        					<li><a href="#">Phone Neil</a></li>
+        					<li><a href="tel:07942759689">Phone Dave</a></li>
+        					<li><a href="tel:07942759739">Phone Neil</a></li>
         					<li class="divider"></li>
         					<li><a href="#">Email Us</a></li>
         					<li class="divider"></li>
