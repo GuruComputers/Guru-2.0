a64c5e223ad846f8a074487ca402b6ba02ac552f
Rev Neil Wakely <neil@gurucomputers.co.uk>
2015-02-24 21:11:24 +0000

    removed nav to seperate file

--- cd.php
+++ cd.php
@@ -7,7 +7,7 @@
     <title>Guru Computers Ltd</title>
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/cd.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -17,80 +17,7 @@
     <![endif]-->
   </head>
   <body>
-    <nav class="navbar navbar-default">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">
-                    <img alt="Guru Computers Ltd" src="img/logo.png" class="navbar-logo">
-                </a>
-            </div>
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav nav-pills">
-                    <li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
-                    <li><a href="#">About</a></li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Contact Us <span class="caret"></span></a>
-                        <ul class="dropdown-menu" role="menu">
-                            <li><a href="tel:07942759689"><span class="glyphicon glyphicon-earphone" aria-hidden="true"></span> Dave (07942 759689)</a></li>
-                            <li><a href="tel:07942759739"><span class="glyphicon glyphicon-earphone" aria-hidden="true"></span> Neil (07942 759739)</a></li>
-                            <li class="divider"></li>
-                            <li><a href="mailto:sales@gurucomputers.co.uk"><img src="./img/email.jpg" class="menu-icons">Email Us</a></li>
-                            <li class="divider"></li>
-                            <li><a href="http:///www.twitter.com/gurucomputersuk" target="_blank"><img src="./img/twitter.png" class="menu-icons">Follow us on Twitter</a></li>
-                            <li class="divider"></li>
-                            <li><a href="http:///www.facebook.com/gurucomputersuk" target="_blank"><img src="./img/facebook.png" class="menu-icons">Add us on Facebook</a></li>
-                        </ul>
-                    </li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Repairs <span class="caret"></span></a>
-                        <ul class="dropdown-menu" role="menu">
-                            <li><a href="#">Computer Repairs</a></li>
-                            <li><a href="#">Mobile Repairs</a></li>
-                            <li class="divider"></li>
-                            <li><a href="#">Book Now</a></li>
-                        </ul>
-                    </li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Websites <span class="caret"></span></a>
-                        <ul class="dropdown-menu" role="menu">
-                            <li><a href="#">Website Development</a></li>
-                            <li><a href="#">Website Hosting</a></li>
-                            <li class="divider"></li>
-                            <li><a href="#">Our Portfolio</a></li>
-                        </ul>
-                    </li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Printing <span class="caret"></span></a>
-                        <ul class="dropdown-menu" role="menu">
-                            <li><a href="cd.php">CD Printing & Duplication</a></li>
-                            <li class="divider"></li>
-                            <li><a href="#">Flyers</a></li>
-                            <li><a href="#">Business Cards</a></li>
-                            <li><a href="#">Letterheads</a></li>
-                            <li class="divider"></li>
-                            <li><a href="#">General Printing</a></li>
-                            <li class="divider"></li>
-                            <li><a href="#">Our Portfolio</a></li>
-                        </ul>
-                    </li>
-                    <li><a href="http://www.gurucomputers.co.uk/shop" target="_blank">Shop</a></li>
-                    <li><a href="http://www.gurucomputers.co.uk/blog" target="_blank">Blog</a></li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Testimonials <span class="caret"></span></a>
-                        <ul class="dropdown-menu" role="menu">
-                            <li><a href="#">View Testimonials</a></li>
-                            <li><a href="#">Add a Testimonial</a></li>
-                        </ul>
-                    </li>
-                </ul>
-        </div>
-    </nav>
+    <?php include "nav.php"; ?>
 
     <div class="wrapper">
         <div class="container">
@@ -99,11 +26,13 @@
 
                     <div class="col-md-10 column">
                         <div class="panel panel-info">
-                            <div class="panel-heading">Need a New Website?</div>
+                            <div class="panel-heading">Need some CD's printed?</div>
                             <div class="panel-body">
-                                <p>Our In-House Team handle all regular aspects of designing, hand-coding and hosting your website so you can relax knowing your Web Presence is in safe hands!</P>
-                                <p>We are able to offer Virtual Private Servers from £10 a month, fully managed by our in-house engineers. We can also provide MySQL and Email Hosting within that price. This means you can have a single point of contact for your whole Web Presence, leaving you more time to run your business without the need to get involved with the technical aspects of your Website.</p>
-                                <p>Our Websites use the following Web Technologies:-</p>
+                                <p>Our In-House CD printing is what you need.</P>
+                                <p>We are able to print your CD's on a range of full face printable CD's.</p>
+                                <p>Due to the CD's being printed in-house we are able to get the fastest turn around possible.</p>
+                                <p>Need help in the design?    That's no problem our professional team are able to tweak your graphics to enable the best results.</p>
+                                <br/>
                                 <ul>
                                     <li>HTML5</li>
                                     <li>CSS3</li>
---
+++ nav.php
@@ -0,0 +1,74 @@
+<nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">
+                    <img alt="Guru Computers Ltd" src="img/logo.png" class="navbar-logo">
+                </a>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav nav-pills">
+                    <li><a href="#">Home <span class="sr-only">(current)</span></a></li>
+                    <li><a href="#">About</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Contact Us <span class="caret"></span></a>
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="tel:07942759689"><span class="glyphicon glyphicon-earphone" aria-hidden="true"></span> Dave (07942 759689)</a></li>
+                            <li><a href="tel:07942759739"><span class="glyphicon glyphicon-earphone" aria-hidden="true"></span> Neil (07942 759739)</a></li>
+                            <li class="divider"></li>
+                            <li><a href="mailto:sales@gurucomputers.co.uk"><img src="./img/email.jpg" class="menu-icons">Email Us</a></li>
+                            <li class="divider"></li>
+                            <li><a href="http:///www.twitter.com/gurucomputersuk" target="_blank"><img src="./img/twitter.png" class="menu-icons">Follow us on Twitter</a></li>
+                            <li class="divider"></li>
+                            <li><a href="http:///www.facebook.com/gurucomputersuk" target="_blank"><img src="./img/facebook.png" class="menu-icons">Add us on Facebook</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Repairs <span class="caret"></span></a>
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="#">Computer Repairs</a></li>
+                            <li><a href="#">Mobile Repairs</a></li>
+                            <li class="divider"></li>
+                            <li><a href="#">Book Now</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Websites <span class="caret"></span></a>
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="#">Website Development</a></li>
+                            <li><a href="#">Website Hosting</a></li>
+                            <li class="divider"></li>
+                            <li><a href="#">Our Portfolio</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown active">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Printing <span class="caret"></span></a>
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="cd.php">CD Printing & Duplication</a></li>
+                            <li class="divider"></li>
+                            <li><a href="#">Flyers</a></li>
+                            <li><a href="#">Business Cards</a></li>
+                            <li><a href="#">Letterheads</a></li>
+                            <li class="divider"></li>
+                            <li><a href="#">General Printing</a></li>
+                            <li class="divider"></li>
+                            <li><a href="#">Our Portfolio</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="http://www.gurucomputers.co.uk/shop" target="_blank">Shop</a></li>
+                    <li><a href="http://www.gurucomputers.co.uk/blog" target="_blank">Blog</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Testimonials <span class="caret"></span></a>
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="#">View Testimonials</a></li>
+                            <li><a href="#">Add a Testimonial</a></li>
+                        </ul>
+                    </li>
+                </ul>
+        </div>
+    </nav>
