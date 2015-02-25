35fa8097dee2fa51ae08d42257f15ccf6e8b91b4
Rev Dave Wakely <dave@gurucomputers.co.uk>
2015-02-25 20:47:18 +0000

    Change Comp Repairs Text

--- comprepairs.php
+++ comprepairs.php
@@ -49,9 +49,10 @@
                 <div class="panel panel-info">
                     <div class="panel-heading">PC Upgrades</div>
                     <div class="panel-body text-center">
-                        <p>Before we get stuck into coding your website, we ensure we have developed a secure, high performance platform for your website to run on.</p>
-                        <p>We utilize the latest CSS3, SASS, LESS, HTML5 and jQuery Techniques to ensure your website works well on any device.</p>
-                        <p>We also make sure that all Fonts, Images and other User Interface Elements are responsive to the best standard current web technologies allow for.</p>
+                        <p>Guru Computers Ltd are specialists in PC upgrades and replacing PC components on your machine. Want your laptop memory improved? No problem, we have everything you need to give your laptop that extra kick in terms of performance.</p> 
+                        <p>Maybe it’s your PC memory that has an issue, in which case you can explore the range of memory options we have for desktop machines.</p>
+                        <p>In addition to our memory options we offer a range of component hardware to improve your PC’s performance. Our CD, DVD and Blu-ray drives are perfect for those who enjoy multimedia on their computers, and our internal hard drives are manufactured with up to 6TB of storage space!</p>
+                        <p>We could even build a PC from scratch with your own spec if you would rather have something different to a standard spec machine!.</p>
                     </div>
                 </div>
             </div>
@@ -59,9 +60,9 @@
                 <div class="panel panel-info">
                     <div class="panel-heading">Operating System Reinstalls</div>
                     <div class="panel-body text-center">
-                        <p>Your website is only a success when it can be navigated with ease, so we perform rigourous testing throughout the development process.</p>
-                        <p>We test your website thoughout the build on several different browsers and devices to ensure maxiumum compatibility. Then we check to make sure there are no vulnerabilities!</p>
-                        <p>During this testing phase, we may invite you to test the website yourselves for ease-of-use.</p>
+                        <p>Sometimes your operating system can get so damaged overtime, the only proper solution is to wipe the machine and start again.</p>
+                        <p>At Guru Computers Ltd, we appreciate that your machine may have important documents, family photos and your favourite music tracks that you'd rather not use. That is why we backup every bit of data possible to ensure your data is safe. We then wipe your Hard drive and restore your machine back to factory settings before putting all your data back on your machine.</p>
+                        <p>However, unlike most computer shops, our service doesn't stop there! We will install all drivers, Windows Updates and Freeware software you originally had on your machine so you can continue using your machine where you left off. Sounds good? Then book today!</p>
                     </div>
                 </div>
             </div>
@@ -71,9 +72,9 @@
                 <div class="panel panel-info">
                     <div class="panel-heading">Laptop Screen Replacements</div>
                     <div class="panel-body text-center">
-                        <p>Our design mockups and wireframes are presented in Mobile, Tablet & Desktop Formats.</p>
-                        <p>These mockups allow us to work with you to make sure your visitors don't miss any important information when they use a smaller device.</p>
-                        <p>We then adapt the website between these device designs to make sure a 7" tablet shows the same as a 10.1" so your website is truely fluid and responsive.</p>
+                        <p>We've all done it, dropped our laptops and unfortuanetly it only takes one unlucky drop and your screen is ruined.</p>
+                        <p>With our Laptop Screen Replacement Service, we can order the screen in from our suppliers in 2-3 days and then fit your brand new screen in a single day.</p>
+                        <p>But it doesn't end there, if you Live or Work in the Brighton Area, we can even fit the screen on-site for you to minimize the time you are left without your machine!</p>
                     </div>
                 </div>
             </div>
@@ -81,9 +82,11 @@
                 <div class="panel panel-info">
                     <div class="panel-heading">Remote Support</div>
                     <div class="panel-body text-center">
-                        <p>Before we get stuck into coding your website, we ensure we have developed a secure, high performance platform for your website to run on.</p>
-                        <p>We utilize the latest CSS3, SASS, LESS, HTML5 and jQuery Techniques to ensure your website works well on any device.</p>
-                        <p>We also make sure that all Fonts, Images and other User Interface Elements are responsive to the best standard current web technologies allow for.</p>
+                        <p>Sometimes you may just need a helping hand to install your new printer, a new piece of software or maybe just sending an email.</p>
+                        <p>For just £5 a month, one of our trained engineers can be assigned to help you remotely should you need it.</p>
+                        <p>All you need to do is topup your Guru USB Support Dongle with £5 a month, and then simply plug it in to your machine when you need help.</p>
+                        <p>Our secure Remote Desktop Software will then generate a one-time use passcode that you simply call us with and we will be able to login and help you.</p>
+                        <p>For your own protection, you can even sit and watch as the engineer operates your machine. When they are done, simply unplug the dongle and keep it in a safe place ready for the next time you need it!</p>
                     </div>
                 </div>
             </div>
@@ -91,9 +94,9 @@
                 <div class="panel panel-info">
                     <div class="panel-heading">Data Recovery</div>
                     <div class="panel-body text-center">
-                        <p>Your website is only a success when it can be navigated with ease, so we perform rigourous testing throughout the development process.</p>
-                        <p>We test your website thoughout the build on several different browsers and devices to ensure maxiumum compatibility. Then we check to make sure there are no vulnerabilities!</p>
-                        <p>During this testing phase, we may invite you to test the website yourselves for ease-of-use.</p>
+                        <p>Deleted a file by accident and now can't get it back?</p>
+                        <p>Give us a call, we may just be able to get that file back! We use state of the art software to recover files lost by deleting, formatting, partition loss, imporper operations etc.</p>
+                        <p>We can recover lost files from hard drive, external hard drive, USB drive, Memory card, digital camera, mobile phone, music player and other storage media, so why not give us a try today?</p>
                     </div>
                 </div>
             </div>
