8f432b6feac793e35d087626cf9b16c71254fb50
Rev Dave Wakely <dave@gurucomputers.co.uk>
2015-02-25 12:28:49 +0000

    Bug Fix for Web Portfolio

--- webportfolio.php
+++ webportfolio.php
@@ -30,7 +30,7 @@
                         foreach ($files as $file){
                             if($file != '.' && $file != '..'){
                                 $withoutExt = preg_replace('/\\.[^.\\s]{3,4}$/', '', $file);
-                                echo '<li class="portfolio_item"><a href="http://www.'.$withoutExt.'" target="_blank">'."\n".'<img src="images/'.$file.'" alt="Portfolio Image">'."\n";
+                                echo '<li class="portfolio_item"><a href="http://www.'.$withoutExt.'" target="_blank">'."\n".'<img src="img/web/'.$file.'" alt="Portfolio Image">'."\n";
                                 echo '<h2>www.'.$withoutExt.'</h2></a></li>'."\n";
                             }
                         }
